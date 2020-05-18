16dc034e48e4e534b163a84251d39badf1747424
Andre Issa <andrelouisissa@gmail.com>
2020-05-19 01:10:22 +1000

    Update ColourPicker.js

--- Userscripts/Desmos/ColourPicker.js
+++ Userscripts/Desmos/ColourPicker.js
@@ -5,6 +5,28 @@
 // @require      https://raw.githubusercontent.com/EastDesire/jscolor/master/jscolor.js
 // ==/UserScript==
 
+/*
+How to use:
+- Use chrome or firefox
+- Install Tampermonkey
+- Install the script
+- Load desmos
+- Reload until there's a box at the top that displays `FFFFFF
+(if it displays `ffffff` it's broken)
+- Click the box at the top to open the picker
+- Move around and click to select colours
+- Type/backspace and enter to change the colour directly
+- Click the save box to the right to save the colour
+- Saved colours appear when you press the cog icon at the top of expressions list and click the coloured circle for a given expression
+
+When you save a desmos graph using custom colours the colours on the expressions will save!
+(but they will need to be added to the colours list if you want to use them for new expressions after reloading)
+
+The script also adds some special colours when loaded, if you don't want these, remove the lines from the script.
+
+If having trouble getting it to load correctly, try changing the `0` at the bottom of the script to something else like `300`
+*/
+
 setTimeout(function() {
     'use strict';
 
