12b7f81424ad8ba711838efce037ded4abc06de4
Andre Issa <andrelouisissa@gmail.com>
2020-08-01 19:43:33 +1000

    Update ColourPicker.js

--- Userscripts/Desmos/ColourPicker.js
+++ Userscripts/Desmos/ColourPicker.js
@@ -29,16 +29,6 @@ If having trouble getting it to load correctly, try changing the `0` at the bott
 setTimeout(function() {
     'use strict';
 
-    function rgb2hex(rgb) {
-        if (/^#[0-9A-F]{6}$/i.test(rgb)) return rgb;
-
-        rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-        function hex(x) {
-            return ("0" + parseInt(x).toString(16)).slice(-2);
-        }
-        return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-    }
-
     var target = document.getElementsByClassName("dcg-header-desktop")[0];
     var hold = document.createElement ('span');
     target.insertBefore(hold, target.childNodes[2])
